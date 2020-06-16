631b9641d769ad9b72b832a17a2bdd2ac2e9ea44
Timur Nazarov <timnazarovya@gmail.com>
2020-06-16 11:24:54 +0300

    Create Face_Ident.py

---
+++ Face_Ident/Face_Ident.py
@@ -0,0 +1,8 @@
+from deepface import DeepFace
+import re
+
+df = DeepFace.find(img_path = "C:/Users/timna/Unknown_Faces/Elon_Musk.jpg", db_path = "C:/Users/timna/Known_Faces",model_name='Facenet')
+if len(df)==0:
+    print('I do not know who is it!')
+else:
+    print('It is probably {}!'.format((re.search('.*\/.*\/.*\/.*[\\\/](.*)\/.*',df.loc[0,'identity'])).group(1))) #Выводится изображение на экран
